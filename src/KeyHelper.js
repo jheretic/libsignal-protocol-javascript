467aae4b363555e1e2dfb366179888996e0fd18c
Josh King <jking@chambana.net>
2016-12-07 13:29:02 -0500

    Initial pass changing namespacing for KeyHelper.js

--- src/KeyHelper.js
+++ src/KeyHelper.js
@@ -1,14 +1,16 @@
+var crypto_wrapper = require("./crypto.js");
+
 function isNonNegativeInteger(n) {
     return (typeof n === 'number' && (n % 1) === 0  && n >= 0);
 }
 
 var KeyHelper = {
     generateIdentityKeyPair: function() {
-        return Internal.crypto.createKeyPair();
+        return crypto_wrapper.crypto.createKeyPair();
     },
 
     generateRegistrationId: function() {
-        var registrationId = new Uint16Array(Internal.crypto.getRandomBytes(2))[0];
+        var registrationId = new Uint16Array(crypto_wrapper.crypto.getRandomBytes(2))[0];
         return registrationId & 0x3fff;
     },
 
@@ -25,8 +27,8 @@ var KeyHelper = {
             );
         }
 
-        return Internal.crypto.createKeyPair().then(function(keyPair) {
-            return Internal.crypto.Ed25519Sign(identityKeyPair.privKey, keyPair.pubKey).then(function(sig) {
+        return crypto_wrapper.crypto.createKeyPair().then(function(keyPair) {
+            return crypto_wrapper.crypto.Ed25519Sign(identityKeyPair.privKey, keyPair.pubKey).then(function(sig) {
                 return {
                     keyId      : signedKeyId,
                     keyPair    : keyPair,
@@ -41,7 +43,7 @@ var KeyHelper = {
             throw new TypeError('Invalid argument for keyId: ' + keyId);
         }
 
-        return Internal.crypto.createKeyPair().then(function(keyPair) {
+        return crypto_wrapper.crypto.createKeyPair().then(function(keyPair) {
             return { keyId: keyId, keyPair: keyPair };
         });
     }
