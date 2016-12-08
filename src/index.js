02009c09d8e6ce5150d367322233c4281162e653
Josh King <jking@chambana.net>
2016-12-08 13:17:29 -0500

    Add index.js to re-export API.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+var address = require("./SignalProtocolAddress.js");
+var curve = require("./Curve.js");
+var keyhelper = require("./KeyHelper.js");
+var fingerprint = require("./NumericFingerprint.js");
+var builder = require("./SessionBuilder.js");
+var cipher = require("./SessionCipher.js");
+var crypto = require("./crypto.js");
+var manager = require("./curve25519_worker_manager.js");
+
+module.exports = exports = {
+    SignalProtocolAddress: address.SignalProtocolAddress,
+    Curve: curve.Curve,
+    KeyHelper: keyhelper.KeyHelper,
+    FingerprintGenerator: fingerprint.FingerprintGenerator,
+    SessionBuilder: builder.SessionBuilder,
+    SessionCipher: cipher.SessionCipher,
+    crypto: crypto.crypto,
+    HKDF: crypto.HKDF,
+    worker: manager.worker
+}
